a821329f0d012580e70942e82828549591c4b513
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-14 10:54:01 -0500

    class Officer created

---
+++ src/main/java/com/galvanize/startrekcaptaindatabase/entities/Officer.java
@@ -0,0 +1,11 @@
+package com.galvanize.startrekcaptaindatabase.entities;
+
+import com.galvanize.startrekcaptaindatabase.Rank;
+
+public class Officer {
+    private long id;
+    private Rank rank;
+    String first;
+    String last;
+}
+

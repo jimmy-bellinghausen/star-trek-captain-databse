f9211f9f0ca167dca1c9322b1f1b5e4ee7da609f
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-14 11:04:38 -0500

    JdbcOfficerDao and test made. data and schema sql files added

---
+++ src/main/java/com/galvanize/startrekcaptaindatabase/JdbcOfficerDao.java
@@ -0,0 +1,15 @@
+package com.galvanize.startrekcaptaindatabase;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcOfficerDao {
+    JdbcTemplate jdbcTemplate;
+
+    public JdbcOfficerDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+}
---
+++ src/test/java/com/galvanize/startrekcaptaindatabase/JdbcOfficerDaoTest.java
@@ -0,0 +1,13 @@
+package com.galvanize.startrekcaptaindatabase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JdbcOfficerDaoTest {
+    @Autowired
+    JdbcOfficerDao jdbcOfficerDao;
+
+}

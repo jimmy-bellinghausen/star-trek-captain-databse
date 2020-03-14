d8147a32a3b1231d2b7c45ee6f61771ce4352cfe
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-14 10:49:21 -0500

    initial commit

---
+++ src/main/java/com/galvanize/startrekcaptaindatabase/StarTrekCaptainDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.startrekcaptaindatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTrekCaptainDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarTrekCaptainDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/startrekcaptaindatabase/StarTrekCaptainDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.startrekcaptaindatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTrekCaptainDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

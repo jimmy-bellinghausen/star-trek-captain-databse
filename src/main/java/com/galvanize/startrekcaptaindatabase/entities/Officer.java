795804ea9fa1c4ad86ed30c49f229a5d5546fbfb
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-14 10:55:49 -0500

    Officer constructors created

--- src/main/java/com/galvanize/startrekcaptaindatabase/entities/Officer.java
+++ src/main/java/com/galvanize/startrekcaptaindatabase/entities/Officer.java
@@ -5,7 +5,24 @@ import com.galvanize.startrekcaptaindatabase.Rank;
 public class Officer {
     private long id;
     private Rank rank;
-    String first;
-    String last;
+    private String first;
+    private String last;
+
+    public Officer(){}
+
+    public Officer(Rank rank, String first, String last) {
+        this.rank = rank;
+        this.first = first;
+        this.last = last;
+    }
+
+    public Officer(long id, Rank rank, String first, String last) {
+        this.id = id;
+        this.rank = rank;
+        this.first = first;
+        this.last = last;
+    }
+
+
 }
 
